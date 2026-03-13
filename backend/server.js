5d5ce71673fdaab7b8ef84835cf721804f4a72e1
luischuy1987-svg <luis.chuy1987@gmail.com>
2026-03-13 12:11:58 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/insertar",(req,res)=>{
 
-const {nombre,edad,correo} = req.body;
+const {nombre,edad,correo}=req.body;
 
+console.log("Datos recibidos:");
 console.log(nombre,edad,correo);
 
-res.send("Datos recibidos");
+res.send("Datos recibidos correctamente");
 
 });
 
 app.listen(3000,()=>{
-console.log("Servidor funcionando");
+console.log("Servidor corriendo en puerto 3000");
 });
