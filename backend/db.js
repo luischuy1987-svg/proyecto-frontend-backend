5a0a2ae0d901758c9d9bd0fcddf7b60f86caec40
luischuy1987-svg <luis.chuy1987@gmail.com>
2026-03-13 12:21:06 -0600

    Set up MySQL connection in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,20 @@
 const mysql = require("mysql2");
+
+const conexion = mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"",
+database:"escuela"
+});
+
+conexion.connect(function(error){
+
+if(error){
+console.log("Error de conexión");
+}else{
+console.log("Conectado a la base de datos");
+}
+
+});
+
+module.exports = conexion;
